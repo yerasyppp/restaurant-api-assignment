3d43473d4f6e39bf4f5b3717ec90d546b7ce6656
Yerasyl <irangaityerasyl@gmail.com>
2026-01-24 23:10:54 +0500

    feat(exception): implement custom exception hierarchy

--- src/com/restaurant/exception/DatabaseOperationException.java
+++ src/com/restaurant/exception/DatabaseOperationException.java
@@ -1,4 +1,7 @@
 package com.restaurant.exception;
 
-public class DatabaseOperationException {
+public class DatabaseOperationException extends RuntimeException {
+    public DatabaseOperationException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
--- src/com/restaurant/exception/InvalidInputException.java
+++ src/com/restaurant/exception/InvalidInputException.java
@@ -1,4 +1,7 @@
 package com.restaurant.exception;
 
-public class InvalidInputException {
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) {
+        super(message);
+    }
 }
